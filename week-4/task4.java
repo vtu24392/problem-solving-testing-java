2e790455becdd965fa40f5052bc330d78409dbc1
vtu24392 <vtu24392@veltech.edu.in>
2026-02-05 14:10:23 +0530

    Implement containsDuplicate method for Leetcode 217

---
+++ week-4/task4.java
@@ -0,0 +1,12 @@
+//Leetcode: 217. CONTAINS DUPLICATE
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i++)
+            if(nums[i]==nums[i-1]) return true;
+      
+        return false;
+    }
+}
